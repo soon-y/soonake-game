0648bc8d0daeca034198208fe37c0dbe79b468b7
Soonyoung Park <soon.syp@gmail.com>
2023-02-14 18:51:29 +0100

    fix swiping only for touchDevice

--- src/Application/World/World.js
+++ src/Application/World/World.js
@@ -256,24 +256,32 @@ function arrowKey(event) {
 let hammertime = new Hammer(canvas);
 hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 hammertime.on('swipe', function (ev) {
-    msg.visible = false
+    swipeSnake
 })
-hammertime.on('swipeleft', function (ev) {
-    if (!ignore) goLeft()
-    setIgnore()
-})
-hammertime.on('swiperight', function (ev) {
-    if (!ignore) goRight()
-    setIgnore()
-})
-hammertime.on('swipeup', function (ev) {
-    if (!ignore) goUp()
-    setIgnore()
-})
-hammertime.on('swipedown', function (ev) {
-    if(!ignore) goDown()
-    setIgnore()
-})
+
+function swipeSnake(){
+    if (isTouchDevice){
+        hammertime.on('swipe', function (ev) {
+            msg.visible = false
+        })
+        hammertime.on('swipeleft', function (ev) {
+            if (!ignore) goLeft()
+            setIgnore()
+        })
+        hammertime.on('swiperight', function (ev) {
+            if (!ignore) goRight()
+            setIgnore()
+        })
+        hammertime.on('swipeup', function (ev) {
+            if (!ignore) goUp()
+            setIgnore()
+        })
+        hammertime.on('swipedown', function (ev) {
+            if(!ignore) goDown()
+            setIgnore()
+        })
+    }
+}
 
 function goLeft() {
     snake.children[0].rotation.y = -Math.PI / 2
