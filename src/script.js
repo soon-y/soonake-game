826ccfbfc91a3a7b2ec31e03f0ba00fd75f6b198
Soonyoung Park <soon.syp@gmail.com>
2023-02-19 10:28:35 +0100

    add sound

--- src/script.js
+++ src/script.js
@@ -1,6 +1,15 @@
+import * as THREE from 'three'
 import Application from './Application/Application.js'
 
 const startButton = document.getElementById( 'startButton' );
 startButton.addEventListener( 'click', () =>{
     const application = new Application(document.querySelector('canvas.webgl'))
-} );
+    const audioListener = new THREE.AudioListener();
+    const sound = new THREE.Audio( audioListener );
+    const loader = new THREE.AudioLoader();
+    loader.load('./sound/appleBite.wav', (buffer) => {
+            sound.setBuffer( buffer );
+            sound.play();
+        }
+    )
+})
