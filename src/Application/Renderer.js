0747e3ec8be8e05d02cb59c5f4d6b608688f2082
Soonyoung Park <soon.syp@gmail.com>
2024-05-08 01:44:14 +0200

    remove camera position

--- src/Application/Renderer.js
+++ src/Application/Renderer.js
@@ -1,59 +1,45 @@
-import Application from "./Application"
-import * as THREE from 'three'
+import Application from "./Application";
+import * as THREE from "three";
 
-export default class Renderer
-{
-    constructor()
-    {
-        this.application = new Application()
-        this.size = this.application.sizes
-        this.scene = this.application.scene
-        this.canvas = this.application.canvas
-        this.camera = this.application.camera
-        this.rtCamera = this.application.rtCamera
+export default class Renderer {
+  constructor() {
+    this.application = new Application();
+    this.size = this.application.sizes;
+    this.scene = this.application.scene;
+    this.canvas = this.application.canvas;
+    this.camera = this.application.camera;
+    this.rtCamera = this.application.rtCamera;
 
-        this.setInstance()
-    }
+    this.setInstance();
+  }
 
-    setInstance()
-    {
-        this.instance = new THREE.WebGLRenderer({
-            canvas: this.canvas,
-            antialias: true
-        })
-        this.instance.physicallyCorrectLights = true
-        this.instance.outputEncoding = THREE.sRGBEncoding
-        this.instance.toneMapping = THREE.CineonToneMapping
-        this.instance.toneMappingExposure = 1.75
-        this.instance.shadowMap.enabled = true
-        this.instance.shadowMap.type = THREE.PCFSoftShadowMap
-        this.instance.setClearColor('#000000')
-        this.instance.setSize(this.size.width, this.size.height)
-        this.instance.setPixelRatio(this.size.pixelRatio)
+  setInstance() {
+    this.instance = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+    this.instance.physicallyCorrectLights = true;
+    this.instance.outputEncoding = THREE.sRGBEncoding;
+    this.instance.toneMapping = THREE.CineonToneMapping;
+    this.instance.toneMappingExposure = 1.75;
+    this.instance.shadowMap.enabled = true;
+    this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.instance.setClearColor("#000000");
+    this.instance.setSize(this.size.width, this.size.height);
+    this.instance.setPixelRatio(this.size.pixelRatio);
 
-        this.aspect = this.size.width/this.size.height
-        if(this.aspect<1){
-            this.camera.instance.position.set(0, 14, 8)
-        }
-    }
+    this.aspect = this.size.width / this.size.height;
+  }
 
-    resize()
-    {
-        this.instance.setSize(this.size.width, this.size.height)
-        this.instance.setPixelRatio(this.size.pixelRatio)
+  resize() {
+    this.instance.setSize(this.size.width, this.size.height);
+    this.instance.setPixelRatio(this.size.pixelRatio);
+  }
 
-        this.aspect = this.size.width/this.size.height
-        if(this.aspect<1){
-            this.camera.instance.position.set(0, 14, 8)
-        }else{
-            this.camera.instance.position.set(0, 10, 6)
-        }
-    }
-
-    update(){
-        this.instance.setRenderTarget(this.rtCamera.rt)
-        this.instance.render(this.scene, this.rtCamera.instance)
-        this.instance.setRenderTarget(null)
-        this.instance.render(this.scene, this.camera.instance)
-    }
+  update() {
+    this.instance.setRenderTarget(this.rtCamera.rt);
+    this.instance.render(this.scene, this.rtCamera.instance);
+    this.instance.setRenderTarget(null);
+    this.instance.render(this.scene, this.camera.instance);
+  }
 }
