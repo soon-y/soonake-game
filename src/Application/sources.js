4a6bad9a155d2a772e7f2c30bdbc30dab88152d4
Soonyoung Park <soon.syp@gmail.com>
2024-05-11 11:21:05 +0200

    update

--- src/Application/sources.js
+++ src/Application/sources.js
@@ -1,92 +1,170 @@
 export default [
-    {
-        name: 'environmentMapTexture',
-        type: 'cubeTexture',
-        path:
-        [
-            'textures/environmentMap/px.jpg',
-            'textures/environmentMap/nx.jpg',
-            'textures/environmentMap/py.jpg',
-            'textures/environmentMap/ny.jpg',
-            'textures/environmentMap/pz.jpg',
-            'textures/environmentMap/nz.jpg'
-        ]
-    },
-    {
-        name: 'fieldTexture',
-        type: 'texture',
-        path:
-        [
-            'textures/floorMap/FloorsCheckerboard_Diffuse.jpg'
-        ]
-    },   
-    {
-        name: 'fieldRough',
-        type: 'texture',
-        path:
-        [
-            'textures/floorMap/FloorsCheckerboard_Rough.jpg'
-        ]
-    },  
-    {
-        name: 'floorTexture',
-        type: 'texture',
-        path:
-        [
-            'textures/floorMap/floor_diff.jpg'
-        ]
-    },
-    {
-        name: 'floorNormal',
-        type: 'texture',
-        path:
-        [
-            'textures/floorMap/floor_normal.jpg'
-        ]
-    },
-    {
-        name: 'appleDiff',
-        type: 'texture',
-        path: 'textures/appleMap/Apple_BaseColor.jpg'
-    },
-    {
-        name: 'appleRough',
-        type: 'texture',
-        path: 'textures/appleMap/Apple_Roughness.jpg'
-    },
-    {
-        name: 'wall',
-        type: 'gltfModel',
-        path: 'models/wall.glb'
-    },
-    {
-        name: 'head',
-        type: 'gltfModel',
-        path: 'models/head.glb'
-    },
-    {
-        name: 'body1',
-        type: 'gltfModel',
-        path: 'models/body1.glb'
-    },
-    {
-        name: 'body2',
-        type: 'gltfModel',
-        path: 'models/body2.glb'
-    },
-    {
-        name: 'kenpixel',
-        type: 'ttf',
-        path: 'font/ttf/kenpixel.ttf'
-    },
-    {
-        name: 'apple',
-        type: 'objModel',
-        path: 'models/apple.obj'
-    },
-    {
-        name: 'screenImage',
-        type: 'texture',
-        path: 'textures/screenMap/screenImage.jpg'
-    }
-]
+  {
+    name: "environmentMapTexture",
+    type: "cubeTexture",
+    path: [
+      "textures/environmentMap/px.jpg",
+      "textures/environmentMap/nx.jpg",
+      "textures/environmentMap/py.jpg",
+      "textures/environmentMap/ny.jpg",
+      "textures/environmentMap/pz.jpg",
+      "textures/environmentMap/nz.jpg",
+    ],
+  },
+  {
+    name: "head",
+    type: "gltfModel",
+    path: "models/head.glb",
+  },
+  {
+    name: "mouth",
+    type: "gltfModel",
+    path: "models/mouth.glb",
+  },
+  {
+    name: "tongue",
+    type: "gltfModel",
+    path: "models/tongue.glb",
+  },
+  {
+    name: "eyelid",
+    type: "gltfModel",
+    path: "models/eyelid.glb",
+  },
+  {
+    name: "body1",
+    type: "gltfModel",
+    path: "models/body1.glb",
+  },
+  {
+    name: "body2",
+    type: "gltfModel",
+    path: "models/body2.glb",
+  },
+  {
+    name: "kenpixel",
+    type: "ttf",
+    path: "font/ttf/kenpixel.ttf",
+  },
+  {
+    name: "apple",
+    type: "objModel",
+    path: "models/apple.obj",
+  },
+  {
+    name: "fieldSpring",
+    type: "gltfModel",
+    path: "models/fieldSpring.glb",
+  },
+  {
+    name: "fieldSummer",
+    type: "gltfModel",
+    path: "models/fieldSummer.glb",
+  },
+  {
+    name: "fieldFall",
+    type: "gltfModel",
+    path: "models/fieldFall.glb",
+  },
+  {
+    name: "fieldWinter",
+    type: "gltfModel",
+    path: "models/fieldWinter.glb",
+  },
+  {
+    name: "fieldBase",
+    type: "gltfModel",
+    path: "models/fieldBase.glb",
+  },
+  {
+    name: "easterEgg",
+    type: "gltfModel",
+    path: "models/easterEgg.glb",
+  },
+  {
+    name: "watermelon",
+    type: "gltfModel",
+    path: "models/watermelon.glb",
+  },
+  {
+    name: "jackOlantern",
+    type: "gltfModel",
+    path: "models/jackOlantern.glb",
+  },
+  {
+    name: "mug",
+    type: "gltfModel",
+    path: "models/mug.glb",
+  },
+  {
+    name: "bunny",
+    type: "gltfModel",
+    path: "models/bunny.glb",
+  },
+  {
+    name: "sunglasses",
+    type: "gltfModel",
+    path: "models/sunglasses.glb",
+  },
+  {
+    name: "Frankenstein",
+    type: "gltfModel",
+    path: "models/Frankenstein.glb",
+  },
+  {
+    name: "santa",
+    type: "gltfModel",
+    path: "models/santa.glb",
+  },
+  {
+    name: "fenceFall",
+    type: "gltfModel",
+    path: "models/fenceFall.glb",
+  },
+  {
+    name: "fenceSpring",
+    type: "gltfModel",
+    path: "models/fenceSpring.glb",
+  },
+  {
+    name: "wallSummer",
+    type: "gltfModel",
+    path: "models/wallSummer.glb",
+  },
+  {
+    name: "wallWinter",
+    type: "gltfModel",
+    path: "models/wallWinter.glb",
+  },
+  {
+    name: "wallSummerCorner",
+    type: "gltfModel",
+    path: "models/wallSummerCorner.glb",
+  },
+  {
+    name: "wallWinterCorner",
+    type: "gltfModel",
+    path: "models/wallWinterCorner.glb",
+  },
+  {
+    name: "water",
+    type: "texture",
+    path: "textures/waterMap/waternormals.jpg",
+  },
+  {
+    name: "gameover",
+    type: "audio",
+    path: "sound/gameOver.wav",
+  },
+
+  {
+    name: "bite",
+    type: "audio",
+    path: "sound/appleBite.wav",
+  },
+  {
+    name: "gulp",
+    type: "audio",
+    path: "sound/gulp.mp3",
+  },
+];
