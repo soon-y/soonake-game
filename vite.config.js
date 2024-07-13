4f012e87d0448027ebed3b85bc58b7fb31984a42
Soonyoung Park <soon.syp@gmail.com>
2024-07-13 22:59:14 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import restart from 'vite-plugin-restart'
+import glsl from 'vite-plugin-glsl'
+
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
@@ -14,5 +17,10 @@ export default {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
-    }
+    },
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
+        glsl() // Handle shader files
+    ]
 }
